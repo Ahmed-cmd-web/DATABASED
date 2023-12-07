edfbef47fb4082357e33b81250eea0f0181a9f08
MostafaVT <mostafaahmed96320@gmail.com>
2023-12-08 00:44:00 +0200

    Add signup form for advisor
    feat: using FN_AdvisorLogin for login

---
+++ webASP/AdvisorSignupForm.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdvisorSignupForm.aspx.cs" Inherits="webASP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            HI Advisor, Please Log In !<br />
+            <br />
+            ID<br />
+            <asp:TextBox ID="advisor_ID" runat="server" Width="211px"></asp:TextBox>
+            <br />
+            Password<br />
+            <asp:TextBox ID="advisor_password" runat="server" Width="211px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BTN_login" runat="server" OnClick="BTN_login_onClick" Text="login" Width="223px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ webASP/AdvisorSignupForm.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace webASP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BTN_login_onClick(object sender, EventArgs e)
+        {
+            int advisor_id;
+
+            try
+            {
+                advisor_id = Int16.Parse(advisor_ID.Text);
+            }catch(Exception ex)
+            {
+                Response.Write("Input Error : "+ex.Message);
+                return;
+
+            }
+            String pass = advisor_password.Text;
+
+
+
+            string connStr = WebConfigurationManager.ConnectionStrings["Advising_System_DB"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            conn.Open();
+
+            SqlCommand FN_AdvisorLogin = new SqlCommand("FN_AdvisorLogin", conn);
+            FN_AdvisorLogin.CommandType = CommandType.StoredProcedure;
+
+            FN_AdvisorLogin.Parameters.Add(new SqlParameter("@advisor_Id", advisor_id));
+            FN_AdvisorLogin.Parameters.Add(new SqlParameter("@password", pass));
+
+            SqlParameter success = new SqlParameter();
+            success.ParameterName = "@success";
+            success.Direction = ParameterDirection.ReturnValue;
+
+            FN_AdvisorLogin.Parameters.Add(success);
+
+            FN_AdvisorLogin.ExecuteScalar();
+
+            bool returnVal = Convert.ToBoolean(success.Value);
+
+            conn.Close();
+
+            if (returnVal) 
+            {
+                Response.Write("Hello Advisor");
+            }
+            else {
+                Response.Write("Error : Advisor is not found!");
+            }
+        }
+    }
+}
